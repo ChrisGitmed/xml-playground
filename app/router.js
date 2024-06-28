abc960ee6fd4662a71b2ab4db91294a507cd22e6
Chris Gitmed <cgitmed@gmail.com>
2024-06-28 01:08:09 -0700

    Setup basic routes to convert between JS and XML

--- app/router.js
+++ app/router.js
@@ -1,12 +1,63 @@
 import { Router } from 'express';
 const router = Router();
 
+import {
+  create as createXml,
+  convert as convertXml,
+} from 'xmlbuilder2';
+
 
 // Health Check
 router.get(
   '/healthcheck',
   async (req, res) => res.status(200).json({ message: 'Success' }),
 );
+
+
+// JS to XML
+router.get(
+  '/js-to-xml',
+  async (req, res, next) => {
+    // Start with JS
+    const obj = {
+      root: {
+        '@att': 'val',
+        foo: {
+          bar: 'foobar'
+        },
+        baz: {}
+      }
+    };
+
+    // Convert the JS to XML
+    const document = createXml(obj);
+    const xml = document.end({ prettyPrint: true });
+
+    // Return the XML
+    return res
+      .status(200)
+      .type('application/xml')
+      .send(xml)
+  }
+)
+
+
+// XML to JS
+router.get(
+  '/xml-to-js',
+  async (req, res, next) => {
+    // Start with XML
+    const originalXml = '<root att="val"><foo><bar>foobar</bar></foo></root>';
+
+    // Convert the XML to JS
+    const convertedXml = convertXml(originalXml, { format: "object" });
+
+    // Return the JS
+    return res.status(200).json(convertedXml)
+  }
+)
+
+
 
 
 export { router };
