eb833a270ddbd162e3d646c1e876c78e8d297ab2
Chris Gitmed <cgitmed@gmail.com>
2024-06-28 00:49:58 -0700

    Created basic web server

---
+++ app/app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import morgan from 'morgan';
+
+class Application {
+  /// CONSTRUCTOR
+  static app = express()
+    .use(morgan('dev'))
+    .use((req, res) => res.status(404).json({ message: 'Route not found' }));
+
+  /// START
+  static async start (port) {
+    // On any uncaught exception, log the exception to the console
+    process.on('uncaughtException', (e) => console.error('Top-Level exception', e, e.stack));
+
+    // Start the server
+    return new Promise((resolve, reject) => {
+      Application.app.listen(port, async (err) => {
+        if (err) reject(err);
+        console.log(`Server listening on port 4200`);
+        resolve();
+      });
+    });
+  }
+}
+
+await Application.start(4200);
